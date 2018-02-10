2a8870dd8097b15ffd3fe662e6a749b14737438f
Mo0dy <felix.muehlenberend@gmail.com>
2018-02-10 12:38:06 +0100

    added point controle for bezier curves

--- Projects/BezierCurves.py
+++ Projects/BezierCurves.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 background_color = 50, 50, 50
 window_size = 800, 800
-
+margin = 100
 
 
 pg.init()
@@ -13,6 +13,8 @@ screen = pg.display.set_mode(window_size)
 pg.display.set_caption("test")
 clock = pg.time.Clock()
 font = pg.font.SysFont("comicsansms", 10)
+# the index of the selected point
+selection = -1
 
 
 def bezier_curve(p0, p1, p2, amount):
@@ -23,18 +25,36 @@ def bezier_curve(p0, p1, p2, amount):
     return np.stack((points_x, points_y), axis=1)
 
 
+def random_point():
+    point = np.random.rand(2)
+    point[0] = point[0] * (window_size[0] - 2 * margin) + margin
+    point[1] = point[1] * (window_size[1] - 2 * margin) + margin
+    return point
+
+
 def reset():
-    global curve
-    curve = bezier_curve(np.array([0, 0]), np.array([50, 100]), np.array([200, 0]), 10)
+    global curve, points
+    points = [random_point() for _ in range(3)]
+    curve = bezier_curve(points[0], points[1], points[2], 100)
+
+
+def move_selected():
+    global points
+    if selection >= 0:
+        points[selection] = np.array(pg.mouse.get_pos())
 
 
 def update():
-    pass
+    global curve
+    move_selected()
+    curve = bezier_curve(points[0], points[1], points[2], 100)
 
 
 def draw():
-    for p in curve:
-        pg.draw.circle(screen, (255, 255, 255), (int(p[0]), int(p[1])), 1)
+    pg.draw.lines(screen, (255, 255, 255), False, curve, 1)
+
+    for p in points:
+        pg.draw.circle(screen, (230, 80, 20), p.astype(int), 5)
 
     text = font.render("%0.2f" % clock.get_fps(), True, (255, 255, 255))
     screen.blit(text, (10, 10))
@@ -45,15 +65,35 @@ keydown_func = {
 }
 
 
+def select():
+    global selection
+    mouse_pos = pg.mouse.get_pos()
+    for i in range(len(points)):
+        d_x = mouse_pos[0] - points[i][0]
+        d_y = mouse_pos[1] - points[i][1]
+        dist = np.sqrt(d_x ** 2 + d_y ** 2)
+        if dist < 50:
+            selection = i
+
+
+mouse_pressed = False
+
+
 reset()
 loop = True
 while loop:
-    clock.tick()
+    clock.tick(30)
     for e in pg.event.get():
         if e.type == pg.QUIT:
             loop = False
         elif e.type == pg.KEYDOWN:
             keydown_func[e.key]()
+        elif e.type == pg.MOUSEBUTTONDOWN:
+            select()
+            mouse_pressed = True
+        elif e.type == pg.MOUSEBUTTONUP:
+            mouse_pressed = True
+            selection = -1
 
     update()
     screen.fill(background_color)
